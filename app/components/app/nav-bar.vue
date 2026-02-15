9eef1cc0192076612e164a383bf724d710170d3a
AnnaKupryushina <69052687+AnnaKupryushina@users.noreply.github.com>
2026-02-15 12:15:50 +0100

    add theme-toggle (#51)

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,3 +1,6 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div class="navbar bg-primary text-primary-content">
     <div class="navbar-start">
@@ -6,6 +9,7 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
+      <App-theme-toggle />
       <a class="btn btn-accent">Sign in
         <Icon name="tabler:brand-github" size="24" />
       </a>
